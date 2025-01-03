e76470f287b9a822cdbb832f251aaf1b2ef09cf8
FreeNewMan <lutovp@yandex.ru>
2025-01-03 22:50:57 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Portfolio from './Portfolio.js';
+import './App.css';
+
+
+function App() {
+  return (
+     <Portfolio />
+  );
+}
+
+export default App;
+
+
+
---
+++ src/Portfolio.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Toolbar from './Toolbar.js'
+
+
+const filters = ["All", "Websites", "Flayers", "Business Cards"];
+const projects = [{
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
+  category: "Business Cards"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+  category: "Websites"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+  category: "Websites"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
+  category: "Websites"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
+  category: "Business Cards"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
+  category: "Websites"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+  category: "Websites"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+  category: "Business Cards"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
+  category: "Websites"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
+  category: "Flayers"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
+  category: "Websites"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
+  category: "Business Cards"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
+  category: "Websites"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+  category: "Websites"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+  category: "Business Cards"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
+  category: "Websites"
+}, {
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
+  category: "Flayers"
+}];
+
+class Portfolio extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selected: 'All',
+      filteredProjects: projects
+     };
+   }
+
+  render() {
+
+    const renderProjects = (projects) => {
+        return projects.map((project, i) => {
+          return (
+            <div className="project" key={`project-${project.category}-${i}`}>
+              <img src={project.img} alt={`project-${project.category}-${i}`} />
+            </div>
+          );
+        });
+      };
+
+    return (
+      <div>
+        <Toolbar
+          filters={filters}
+          selected={this.state.selected}
+          onSelectFilter={(filter) => {
+            this.setState({ selected: filter });
+            if (filter !== 'All')
+              {this.setState({ filteredProjects: projects.filter(function(item) {return item.category === filter}) })}
+            else { this.setState({filteredProjects: projects })}
+            console.log(filter)
+          }} />
+       <div className="portfolio">
+          {renderProjects(this.state.filteredProjects)}
+      </div>
+      </div>
+    );
+
+  }
+
+}
+
+  export default Portfolio;
